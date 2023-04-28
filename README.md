c987cb1289d498dfaf73f41cc20ed19a1707d7ba
yoshinorin <yoshinorin.net@outlook.com>
2023-04-29 00:13:30 +0900

    add `assertImages` cmd

--- README.md
+++ README.md
@@ -2,15 +2,42 @@
 
 The cli for [Qualtet](https://github.com/yoshinorin/qualtet).
 
-# Setup
+## Setup
 
 Clone this repository at the same hierarchy as the Hexo folder.
 
-# Commands
+## Commands
 
-- TODO
+- assertImages: `node ./cmd/assertImages.js <daysAgo>`
 
-# LICENSE
+### assertImages
+
+Validate an image has EXIF(includes GPS) info or not.
+
+```sh
+$ node ./cmd/assertImages.js <daysAgo (default: 10000)>
+
+INFO  check updated in 10000 days ago articles assets.
+INFO  Validating config
+INFO  Start processing
+ERROR C:\Users\<userName>\source\_posts\example\gps.jpg: has GPS info
+{
+  file: 'C:\Users\<userName>\source\_posts\example\gps.jpg',
+  gps: [
+    undefined,
+    undefined,
+    undefined,
+    { id: 2, value: [Array], description: 39.9987 },
+    { id: 1, value: [Array], description: 'North latitude' },
+    { id: 4, value: [Array], description: 138.6517 },
+    { id: 3, value: [Array], description: 'East longitude' },
+    { id: 18, value: [Array], description: 'WGS-84' }
+  ]
+}
+WARN  C:\Users\<userName>\source\_posts\example2\hoge.jpg: has EXIF
+```
+
+## LICENSE
 
 © yoshinorin
 
---
+++ cmd/assertImages.js
@@ -0,0 +1,46 @@
+const Hexo = require('hexo');
+const hexo = new Hexo(process.cwd(), {silent: false});
+const log = require('hexo-log').default({
+  debug: false,
+  silent: false
+});
+const { validate } = require('../lib/imageValidation');
+
+const daysAgo = process.argv[2] ? process.argv[2] : 10000;
+
+log.info(`check updated in ${daysAgo} days ago articles assets.`)
+
+hexo.init().then(() => {
+  hexo.load().then(() => {
+
+    let date = new Date();
+    date = date.setDate(date.getDate() - daysAgo);
+    const postAsset = hexo.model('PostAsset');
+    const pageAsset = hexo.model('Asset');
+
+    const posts = hexo.locals.get('posts').filter(c => c.updated > date);
+    for (let post of posts.toArray()) {
+      const assets = postAsset.find({post: post._id}).toArray();
+      assets.forEach(a => {
+        if (a.source.endsWith('.pptx') || a.source.endsWith('.svg') || a.source.endsWith('.ico') || a.source.endsWith('.mp3') || a.source.endsWith('.gif')) {
+          // Nothing todo
+        } else {
+          validate(a.source);
+        }
+      });
+    };
+
+    const pages = hexo.locals.get('pages').filter(c => c.updated > date)
+    for(let page of pages.toArray()) {
+      const pageDir = page.path.slice(0, page.path.lastIndexOf("/"));
+      const assets = pageAsset.filter(x => x._id.includes(pageDir));
+      assets.forEach(a => {
+        if (a.source.endsWith('.pptx') || a.source.endsWith('.svg') || a.source.endsWith('.ico') || a.source.endsWith('.mp3') || a.source.endsWith('.gif')) {
+          // Nothing todo
+        } else {
+          validate(a.source);
+        }
+      });
+    };
+  });
+});
