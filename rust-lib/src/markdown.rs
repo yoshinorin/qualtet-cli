1e1c5bc1480542c123a81bed5ac39f623384ea15
yoshinorin <yoshinorin.net@outlook.com>
2024-12-25 11:41:35 +0900

    wip(markdown): impl markdown-it-rs

--- rust-lib/src/markdown.rs
+++ rust-lib/src/markdown.rs
@@ -1,8 +1,13 @@
 use markdown_it;
+use markdown_it_footnote;
 
 // WIP
 pub fn render(input: &str) -> String {
   let parser = &mut markdown_it::MarkdownIt::new();
+  markdown_it::plugins::cmark::add(parser);
+  markdown_it::plugins::extra::add(parser);
+  markdown_it_footnote::add(parser);
+
   let ast = parser.parse(input);
   ast.render()
 }
