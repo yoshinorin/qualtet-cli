c7c9577b8dfb5c90e7c9d6ba4dd26d47991b2219
yoshinorin <yoshinorin.net@outlook.com>
2025-01-01 17:30:03 +0900

    refactor(markdown): use `markdown_it_lazyload`

--- rust-lib/src/markdown.rs
+++ rust-lib/src/markdown.rs
@@ -1,6 +1,5 @@
-use markdown_it::generics::inline::full_link;
-use markdown_it::{self, NodeValue};
 use markdown_it_footnote;
+use markdown_it_lazyload;
 
 // WIP
 pub fn render(input: &str) -> String {
@@ -8,41 +7,10 @@ pub fn render(input: &str) -> String {
   markdown_it::plugins::cmark::add(parser);
   markdown_it::plugins::extra::add(parser);
   markdown_it_footnote::add(parser);
-  add_custom_image(parser);
+  markdown_it_lazyload::add(parser);
 
   let ast = parser.parse(input);
   ast.render()
-}
-
-#[derive(Debug)]
-struct CustomImage {
-  pub url: String,
-  pub title: Option<String>,
-}
-
-impl NodeValue for CustomImage {
-  fn render(&self, node: &markdown_it::Node, fmt: &mut dyn markdown_it::Renderer) {
-    let mut attrs = node.attrs.clone();
-
-    attrs.push(("src", self.url.clone()));
-    attrs.push(("alt", node.collect_text()));
-    attrs.push(("loading", "lazy".to_string()));
-
-    if let Some(title) = &self.title {
-      attrs.push(("title", title.clone()));
-    }
-
-    fmt.self_close("img", &attrs);
-  }
-}
-
-fn add_custom_image(md: &mut markdown_it::MarkdownIt) {
-  full_link::add_prefix::<'!', true>(md, |href, title| {
-    markdown_it::Node::new(CustomImage {
-      url: href.unwrap_or_default(),
-      title,
-    })
-  });
 }
 
 #[cfg(test)]
